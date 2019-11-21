8a78751010dd0a88e77549bde1c1efe0809c559d
Andrew Conley <conley@localhost.localdomain>
2019-11-20 20:39:42 -0500

    added testing files

---
+++ test.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <ctype.h>
+#include <netdb.h>
+#include <pthread.h>
+
+
+
+#define PORT 5000
+#define NUM_CLIENTS 100
+#define NO_CLIENT -1
+#define TIMEOUT 2
+#define NUM_MESSAGES 10000
+#define BUF_SIZE 256
+#define BACKLOG 100
+
+void* sendMessages(void* sockfd){
+	int fd = *((int*)sockfd);
+
+	char* buf = calloc(BUF_SIZE, sizeof(char));
+
+	for(int n = 0; n < NUM_MESSAGES; n++){
+		write(fd, buf, strlen(buf) + 1);
+		memset(buf, 0, BUF_SIZE);
+	}
+
+	free(buf);
+
+	return NULL;
+}
+
+// removes message from read queue
+void recvMessage(int fd){
+	char buf[BUF_SIZE];
+
+	read(fd, buf, BUF_SIZE);
+}
+
+int main(){
+	// file descriptor listening for new connections
+	int listenFd = socket(PF_INET, SOCK_STREAM, 0);
+
+	if(listenFd < 0){
+		perror("socket() failed\n");
+		return EXIT_FAILURE;
+	}
+
+	// holds address and port of server
+	struct sockaddr_in server;
+
+	// zero out sockaddr
+	memset(&server, 0, sizeof(struct sockaddr_in));
+
+	// use IPv4
+	server.sin_family = AF_INET;
+	// allow any ip address
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	// initialize to given port
+	server.sin_port = htons(PORT);
+
+	// size of struct for bind()
+	int len = sizeof(struct sockaddr_in);
+
+	// bind listening server to address 
+	if(bind(listenFd, (struct sockaddr*)&server, len) != 0){
+		perror("bind() failed\n");
+		return EXIT_FAILURE;
+	}
+
+	// set listening socket to listening state
+	if(listen(listenFd, BACKLOG) != 0){
+		perror("listen() failed\n");
+		return EXIT_FAILURE;
+	}
+
+	int* clients = calloc(NUM_CLIENTS, sizeof(int));
+	for(int n = 0; n < NUM_CLIENTS; n++){
+		clients[n] = NO_CLIENT;
+	}
+	pthread_t* clientThreads = calloc(NUM_CLIENTS, sizeof(pthread_t));
+
+	// structure to hold a set of file descriptors to watch
+	fd_set rfds;
+	int maxIndex = 0;
+
+	while(1){
+
+		int maxFd = listenFd;
+
+		// set rfds to include no file descriptors
+		FD_ZERO(&rfds);
+
+		FD_SET(listenFd, &rfds);
+
+		// add any connected clients
+		for(int n = 0; n < NUM_CLIENTS; n++){
+
+			if(clients[n] != NO_CLIENT){
+				FD_SET(clients[n], &rfds);
+				// max fd is max of highest seen and new fd
+				maxFd = maxFd > clients[n] ? maxFd : clients[n];
+			}
+		}
+
+		// structure to specify TIMEOUT second timeout on select() call
+		struct timeval timeout;
+
+		timeout.tv_sec = TIMEOUT;
+		timeout.tv_usec = 1000;
+
+		// wait for activity on listening socket, or any active client
+		int retval = select(maxFd + 1, &rfds, NULL, NULL, &timeout);
+
+		if(retval == 0){
+			//printf("No Activity\n");
+			break;
+			//return 0;
+		}
+		else if(retval == -1){
+			perror("ERROR Select() failed\n");
+			return EXIT_FAILURE;
+		}
+
+		if(FD_ISSET(listenFd, &rfds)){
+			int index = -1;
+			// accept new connection
+
+			for(int n = maxIndex; n < NUM_CLIENTS; n++){
+				if(clients[n] == NO_CLIENT){
+					index = n;
+					maxIndex++;
+					break;
+				}
+			}
+
+			clients[index] = accept(listenFd, NULL, NULL);
+
+			pthread_create(&clientThreads[index], NULL, sendMessages, (void*)&clients[index]);
+		}
+
+		for(int n = 0; n < NUM_CLIENTS; n++){
+			if(FD_ISSET(clients[n], &rfds)){
+				recvMessage(clients[n]);
+			}
+		}
+
+	}
+
+	for(int n = 0; n < NUM_CLIENTS; n++){
+		if(clientThreads[n] != 0){
+			pthread_join(clientThreads[n], NULL);
+		}
+		if(clients[n] != NO_CLIENT){
+			close(clients[n]);
+		}
+	}
+
+	close(listenFd);
+
+	free(clients);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ testClient.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <ctype.h>
+#include <netdb.h>
+
+#define MESSAGE "HELLO FROM CLIENT"
+#define NUM_MESSAGES 10000
+#define BUF_SIZE 256
+#define PORT 5000
+
+int main(){
+
+	// create socket, IPv4, tcp
+	int sdTCP = socket(PF_INET, SOCK_STREAM, 0);
+
+	if(sdTCP == -1){
+		perror("ERROR: Failed to create socket\n");
+		return EXIT_FAILURE;
+	}
+
+	struct sockaddr_in server;
+	/* from stack overflow:
+
+			"So the correct thing to do is to use AF_INET in your 
+			struct sockaddr_in and PF_INET in your call to socket().
+			But practically speaking, you can use AF_INET everywhere."
+
+	*/
+	server.sin_family = AF_INET;
+	// connect to localhost, inet_addr removes periods and turns string into desired format
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	// set server port to which we will connect
+	server.sin_port = htons(PORT);
+
+	socklen_t len = sizeof(server);
+
+	// connect
+	if(connect(sdTCP, (struct sockaddr*)&server, len) != 0){
+		perror("ERROR: connect() failed\n");
+		return EXIT_FAILURE;
+	}
+
+	// fd set for the one fd, to poll
+	fd_set rfds;
+
+	char* readBuf[BUF_SIZE];
+	char* buf = MESSAGE; 
+
+	for(int n = 0; n < NUM_MESSAGES; n++){
+		FD_ZERO(&rfds);
+
+		FD_SET(sdTCP, &rfds);
+
+		// structure to specify TIMEOUT second timeout on select() call
+		struct timeval timeout;
+
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 0;
+
+		select(sdTCP + 1, &rfds, NULL, NULL, &timeout);
+
+
+		write(sdTCP, buf, strlen(buf) + 1);
+		if(FD_ISSET(sdTCP, &rfds)){
+			read(sdTCP, readBuf, BUF_SIZE);
+		}
+	}
+
+	close(sdTCP);
+
+
+}
